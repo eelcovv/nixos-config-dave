542e8f587504b9c3992872921d9ccda7407f754c
Eelco <eelcovv@gmail.com>
2025-01-01 13:58:36 +0100

    added alloy laptop

---
+++ hosts/alloy/variables.nix
@@ -0,0 +1,18 @@
+{
+  # Git Configuration ( For Pulling Software Repos )
+  gitUsername = "Eelco van Vliet";
+  gitEmail = "eelcovv@gmail.com";
+
+  # Hyprland Settings
+  extraMonitorSettings = "";
+
+  # Waybar Settings
+  clock24h = true;
+
+  # Program Options
+  browser = "brave"; # Set Default Browser (google-chrome-stable for google-chrome)
+  terminal = "kitty"; # Set Default System Terminal
+  keyboardLayout = "us";
+
+  timezone = "Europe/Amsterdam";
+}
