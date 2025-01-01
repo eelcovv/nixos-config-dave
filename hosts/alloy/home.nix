c114c4766835c1968354b590b1f37dca1d11a11a
Eelco <eelcovv@gmail.com>
2025-01-01 14:11:54 +0100

    synced with default

--- hosts/alloy/home.nix
+++ hosts/alloy/home.nix
@@ -185,7 +185,7 @@ in
         sv = "sudo nvim";
         fr = "nh os switch --hostname ${host} /home/${username}/zaneyos";
         fu = "nh os switch --hostname ${host} --update /home/${username}/zaneyos";
-        zu = "sh <(curl -L https://gitlab.com/Zaney/zaneyos/-/raw/main/install-zaneyos.sh)";
+        zu = "sh <(curl -L https://raw.githubusercontent.com/eelcovv/nixos-config-dave/refs/heads/main/install-daveos.sh)";
         ncg = "nix-collect-garbage --delete-old && sudo nix-collect-garbage -d && sudo /run/current-system/bin/switch-to-configuration boot";
         v = "nvim";
         cat = "bat";
