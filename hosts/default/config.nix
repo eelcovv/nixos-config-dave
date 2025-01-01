277993ba70228d57553e472c8571889a021b1e7d
Eelco <eelcovv@gmail.com>
2025-01-01 05:53:00 -0600

    fixed variable

--- hosts/default/config.nix
+++ hosts/default/config.nix
@@ -118,7 +118,7 @@ in
   networking.timeServers = options.networking.timeServers.default ++ [ "pool.ntp.org" ];
 
   # Set your time zone.
-  time.timeZone = $timezone;
+  time.timeZone = "${timezone}";
 
 
   # Select internationalisation properties.
